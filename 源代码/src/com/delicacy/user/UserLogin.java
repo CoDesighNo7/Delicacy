c64912e748634f37925ecccf77be3aa40461397c
liushiming <1484447422@qq.com>
2017-06-28 11:22:26 +0800

    用户登录 bean

---
+++ 源代码/src/com/delicacy/user/UserLogin.java
@@ -0,0 +1,20 @@
+package com.delicacy.user;
+
+public class UserLogin {
+	private String userID;
+	private String uerPassword;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUerPassword() {
+		return uerPassword;
+	}
+	public void setUerPassword(String uerPassword) {
+		this.uerPassword = uerPassword;
+	}
+	
+
+}
