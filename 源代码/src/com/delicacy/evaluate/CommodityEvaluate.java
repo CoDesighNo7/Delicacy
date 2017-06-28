ab5077e31467ee4916c3344adf3871008d6909de
liushiming <1484447422@qq.com>
2017-06-28 11:06:11 +0800

    评价部分bean

--- 源代码/src/com/delicacy/evaluate/CommodityEvaluate.java
+++ 源代码/src/com/delicacy/evaluate/CommodityEvaluate.java
@@ -1,5 +1,78 @@
 package com.delicacy.evaluate;
 
+import java.util.Date;
+
 public class CommodityEvaluate {
+	private String commodityName;
+	private String commodityID;
+	private String sourceID;
+	private String userID;
+	private Float describeMark;
+	private Float qualityMark;
+	private Float deliverMark;
+	private String evaluate;
+	private String addevaluate;
+	private Date Time;
+	public String getCommodityName() {
+		return commodityName;
+	}
+	public void setCommodityName(String commodityName) {
+		this.commodityName = commodityName;
+	}
+	public String getCommodityID() {
+		return commodityID;
+	}
+	public void setCommodityID(String commodityID) {
+		this.commodityID = commodityID;
+	}
+	public String getSourceID() {
+		return sourceID;
+	}
+	public void setSourceID(String sourceID) {
+		this.sourceID = sourceID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public Float getDescribeMark() {
+		return describeMark;
+	}
+	public void setDescribeMark(Float describeMark) {
+		this.describeMark = describeMark;
+	}
+	public Float getQualityMark() {
+		return qualityMark;
+	}
+	public void setQualityMark(Float qualityMark) {
+		this.qualityMark = qualityMark;
+	}
+	public Float getDeliverMark() {
+		return deliverMark;
+	}
+	public void setDeliverMark(Float deliverMark) {
+		this.deliverMark = deliverMark;
+	}
+	public String getEvaluate() {
+		return evaluate;
+	}
+	public void setEvaluate(String evaluate) {
+		this.evaluate = evaluate;
+	}
+	public String getAddevaluate() {
+		return addevaluate;
+	}
+	public void setAddevaluate(String addevaluate) {
+		this.addevaluate = addevaluate;
+	}
+	public Date getTime() {
+		return Time;
+	}
+	public void setTime(Date time) {
+		Time = time;
+	}
+	
 
 }
