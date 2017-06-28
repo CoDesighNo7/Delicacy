06f00083e959245e8e0c62d337d7c5afc4462475
liushiming <1484447422@qq.com>
2017-06-28 10:58:30 +0800

    购物车信息bean

--- 源代码/src/com/delicacy/cart/Cart.java
+++ 源代码/src/com/delicacy/cart/Cart.java
@@ -1,5 +1,34 @@
 package com.delicacy.cart;
 
+import java.util.ArrayList;
+
+import com.delicacy.commodity.Commodity_Cart;
+
 public class Cart {
+	private ArrayList<Commodity_Cart> commodityList;
+	private float cartAmont;
+	private String userID;
+	public ArrayList<Commodity_Cart> getCommodityList() {
+		return commodityList;
+	}
+	public void setCommodityList(ArrayList<Commodity_Cart> commodityList) {
+		this.commodityList = commodityList;
+	}
+	public float getCartAmont() {
+		return cartAmont;
+	}
+	public void setCartAmont(float cartAmont) {
+		this.cartAmont = cartAmont;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	
+	
+	
+	
 
 }
